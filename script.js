c1a3e910030ad23dc896259bc714518c9709830a
Nareshb78 <nareshbaghel909@gmail.com>
2025-06-27 19:41:54 +0530

    js

--- script.js
+++ script.js
@@ -44,48 +44,69 @@
 // let arr3 = [400,500];
 // console.log(arr.concat(arr2,arr3));
 
-const laptop = {
-    name:"MSI",
-    screenSize : 16,
-    keyboard: "SteelSeries",
-    mouse :{
-        name:"Cosmic Byte",
-        dpi:1600
-    },
-    change: function(newdpi){
-       this.mouse.dpi= newdpi;
+// const laptop = {
+//     name:"MSI",
+//     screenSize : 16,
+//     keyboard: "SteelSeries",
+//     mouse :{
+//         name:"Cosmic Byte",
+//         dpi:1600
+//     },
+//     change: function(newdpi){
+//        this.mouse.dpi= newdpi;
+//     }
+// };
+
+// let button = "BUTTON";
+
+// let content = `
+//     <main>
+//         <h2>Laptop</h2>
+//         <div ><ul class="container">
+//             <li class="first">Name : ${laptop.name}</li>
+//             <li>ScreenSize : ${laptop.screenSize}</li>
+//             <li>Keyboard : ${laptop.keyboard}</li>
+//             <li>Mouse Name : ${laptop.mouse.name}</li>
+//             <li>Mouse DPI : ${laptop.mouse.dpi}</li>
+//         </ul></div>
+//         <button class="button">
+//                 ${button}
+//         </button>   
+//     </main>
+// `;
+// document.body.innerHTML = content;
+
+// let test = document.querySelector(".button");
+// test.addEventListener("click",(event)=>{
+//     test.classList.add("pop")
+//     if(test.innerHTML == button){
+//         test.innerHTML = "CHANGED";
+//         test.style.backgroundColor = "red";
+//     }
+//     else{
+//         test.innerHTML = button;
+//         test.style.backgroundColor = "blue";
+//     }
+
+// })
+class Base {
+    set field(val) {
+      console.log(val);
     }
-};
-
-let button = "BUTTON";
-
-let content = `
-    <main>
-        <h2>Laptop</h2>
-        <div ><ul class="container">
-            <li class="first">Name : ${laptop.name}</li>
-            <li>ScreenSize : ${laptop.screenSize}</li>
-            <li>Keyboard : ${laptop.keyboard}</li>
-            <li>Mouse Name : ${laptop.mouse.name}</li>
-            <li>Mouse DPI : ${laptop.mouse.dpi}</li>
-        </ul></div>
-        <button class="button">
-                ${button}
-        </button>   
-    </main>
-`;
-document.body.innerHTML = content;
-
-let test = document.querySelector(".button");
-test.addEventListener("click",(event)=>{
-    test.classList.add("pop")
-    if(test.innerHTML == button){
-        test.innerHTML = "CHANGED";
-        test.style.backgroundColor = "red";
+  }
+  
+  class DerivedWithField extends Base {
+    field = 1;
+  }
+  
+  const instance = new DerivedWithField(); // No log
+  
+  class DerivedWithConstructor extends Base {
+    constructor() {
+      super();
+      this.field = 1;
     }
-    else{
-        test.innerHTML = button;
-        test.style.backgroundColor = "blue";
-    }
-
-})
+  }
+  
+  const instance2 = new DerivedWithConstructor(); // Logs 1
+  
